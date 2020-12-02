98126b8381767712bba65a6f6160345af1e4bf29
VitaliiShpital <takur12345@gmail.com>
2020-12-02 22:20:59 +0200

    signup form reworked, app background and nav bar changed

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,27 +1,40 @@
-import React, { useState } from 'react'
-import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+import React, {useEffect, useState} from 'react'
+import {Link} from "react-router-dom";
+import { Checkbox, Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
 
-import ExpandArea from "../components/ExpandArea";
 import './Signup.css'
 
 export default function Signup(props) {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [message, setMessage] = useState('')
-  const [subject, setSubject] = useState('')
-  const [htmlMessage, setHtmlMessage] = useState('')
+  const [isUserNameFieldVisible, setIsUserNameFieldVisible] = useState(false)
+  const [isCheckboxChecked, setIsCheckboxChecked] = useState(false)
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const emailParam = urlParams.get('email')
+    emailParam ? setUsername(emailParam) : setIsUserNameFieldVisible(true)
+  })
 
   function validateForm() {
-    return username.length > 0 && password.length > 0
+    return username.length > 0 && password.length > 0 && confirmPassword.length > 0 && isCheckboxChecked
+  }
+
+  function toggleCheckbox() {
+    isCheckboxChecked ? setIsCheckboxChecked(false) : setIsCheckboxChecked(true)
   }
 
   async function handleSignup(event) {
     event.preventDefault()
 
-    const messageParameters = { message, subject, htmlMessage }
+    if (password !== confirmPassword) {
+      alert('Passwords don\'t match!')
+      return
+    }
 
     try {
-      const token = await window.sdk.signUp(username, password, messageParameters)
+      const token = await window.sdk.signUp(username, password)
 
       const isUsername = !username.startsWith('+') && username.indexOf('@') === -1
 
@@ -40,35 +53,59 @@ export default function Signup(props) {
 
   return (
     <div className='Signup'>
-      <form onSubmit={handleSignup}>
-        <FormGroup controlId='username' bsSize='large'>
-          <ControlLabel>Username</ControlLabel>
-          <FormControl
-            autoFocus
-            type='username'
-            value={username}
-            onChange={ event => setUsername(event.target.value) }
-          />
-        </FormGroup>
+      <form className='Form' onSubmit={handleSignup}>
+        <h1 className='Title'>Create account</h1>
+        <p className='Info'>
+          Welcome to your personal wallet. Here you will be able to store, view and manage your digital identity.
+          <br/><br/>
+          In order to access your wallet you will need to set up a password first
+        </p>
+        { isUserNameFieldVisible &&
+          <FormGroup controlId='username' bsSize='large'>
+            <ControlLabel className='Label'>Username</ControlLabel>
+            <FormControl
+              autoFocus
+              className='Input'
+              type='username'
+              value={username}
+              onChange={ event => setUsername(event.target.value) }
+            />
+          </FormGroup>
+        }
         <FormGroup controlId='password' bsSize='large'>
-          <ControlLabel>Password</ControlLabel>
+          <ControlLabel className='Label'>Password</ControlLabel>
           <FormControl
+            className='Input'
             value={password}
             onChange={ event => setPassword(event.target.value) }
             type='password'
           />
         </FormGroup>
-        <ExpandArea
-          message={message}
-          subject={subject}
-          htmlMessage={htmlMessage}
-          setMessage={setMessage}
-          setSubject={setSubject}
-          setHtmlMessage={setHtmlMessage}
-        />
-        <Button block bsSize='large' disabled={!validateForm()} type='submit'>
+        <FormGroup controlId='confirmPassword' bsSize='large'>
+          <ControlLabel className='Label'>Confirm Password</ControlLabel>
+          <FormControl
+            className='Input'
+            value={confirmPassword}
+            onChange={ event => setConfirmPassword(event.target.value) }
+            type='password'
+          />
+        </FormGroup>
+        <FormGroup controlId='checkbox' bsSize='large'>
+          <Checkbox
+            className='Signup-Checkbox'
+            checked={isCheckboxChecked}
+            onChange={() => toggleCheckbox()}
+          >
+            I accept the terms and conditions
+          </Checkbox>
+        </FormGroup>
+        <Button className='Button' block bsSize='large' disabled={!validateForm()} type='submit'>
           Sign Up
         </Button>
+        <p className='Link-Label'>
+          Already have an account?
+          <Link to='/login' className='Link'>Login</Link>
+        </p>
       </form>
     </div>
   )
